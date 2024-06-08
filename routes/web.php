52bf8b72f49df00e03b0417edf446e33338202a1
Akrez <akrez.like@gmail.com>
2024-06-09 02:14:34 +0330

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\WalletController;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get(RouteServiceProvider::HOME, [HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->prefix('dashboard')->group(function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+
+    Route::prefix('user/{user}')->group(function () {
+
+        Route::resource('wallets', WalletController::class)->only(['index', 'create', 'store', 'edit', 'update']);
+
+        Route::get('wallets/{wallet}/transactions/deposit', [TransactionController::class, 'deposit'])->name('transactions.deposit');
+        Route::get('wallets/{wallet}/transactions/withdraw', [TransactionController::class, 'withdraw'])->name('transactions.withdraw');
+        Route::get('wallets/{wallet}/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+        Route::post('wallets/{wallet}/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    });
 });
